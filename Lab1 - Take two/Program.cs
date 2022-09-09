e39a7a9d103652077419c5fbe887ff3c0f3f1d8a
MagnusSandbergITHS <magnus.sandberg@iths.se>
2022-09-09 12:44:39 +0200

    Add project files.

---
+++ Lab1 - Take two/Program.cs
@@ -0,0 +1,27 @@
+﻿//Användarens textsträng
+Console.Write("Skriv in valfri sekvens: ");
+string userInput = Console.ReadLine();
+
+// Konverterar anvnändarens sträng till en char array
+char[] userInputToChar = userInput.ToCharArray();
+
+string firstOcc = "", secondOcc = "";
+string firstPart = "";
+string singleDigit = "";
+
+for (int i = 0; i < userInput.Length; i++)
+{
+    if (char.IsDigit(userInput[i]))
+    {
+        firstOcc = userInput.Substring(userInput.IndexOf(userInputToChar[i], 0));
+        secondOcc = userInput.Substring(userInput.IndexOf(userInputToChar[i], 1));
+        singleDigit = secondOcc[i].ToString();
+        break;
+    }
+}
+
+string[] firstOccParts = firstOcc.Split(secondOcc);
+
+firstPart += firstOccParts[0] + singleDigit;
+secondOcc += secondOcc - int.Parse(singleDigit);
+Console.WriteLine($"FP: {firstPart}\nSP: {secondOcc}");
